2dcc8151d210ee2f80ab2ef3cb46f95111d17ea1
Arham Javed <81445694+arhamjaved46@users.noreply.github.com>
2023-02-26 03:43:16 +0500

    Complete the navbar, routing, profile-img & sub-menu

--- Fiver UI/src/App.jsx
+++ Fiver UI/src/App.jsx
@@ -1,56 +1,76 @@
 import React from 'react';
 import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Home from './pages/homepage/homepage';
+import AddNewGig from './pages/addGigs/addGig';
+import Gig from './pages/gig/gig';
+import MyGigs from './pages/myGigs/myGigs';
+import Orders from './pages/orders/orders';
+import Message from './pages/message/message';
+import Messages from './pages/messages/messages';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+} from 'react-router-dom';
 import './App.scss';
 
 function App() {
+
+  const Layout = () => {
+    return (
+      <>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: '/gigs',
+          element: <MyGigs />,
+        },
+        {
+          path: '/gig/:id',
+          element: <Gig />,
+        },
+        {
+          path: '/orders',
+          element: <Orders />,
+        },
+        {
+          path: '/mygigs',
+          element: <MyGigs />,
+        },
+        {
+          path: '/addnewgig',
+          element: <AddNewGig />,
+        },
+        {
+          path: '/messages',
+          element: <Messages />,
+        },
+        {
+          path: '/message/:id',
+          element: <Message />,
+        },
+      ],
+    }
+  ]);
+
   return (
     <div>
-      <Navbar />
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
-      <h1>arham javed</h1>
+      <RouterProvider router={router} />
     </div>
   )
 }
--- Fiver UI/src/components/navbar/Navbar.jsx
+++ Fiver UI/src/components/navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
-// import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import './Navbar.scss';
 
 function Navbar() {
 
     const [active, setActive] = useState(false);
-    const [activeBtn, setActiveBtn] = useState(false);
+    const [open, setOpen] = useState(false);
+
+    const { pathname } = useLocation();
 
     const isActive = () => {
         window.scrollY > 2 ? setActive(true) : setActive(false);
@@ -19,40 +21,74 @@ function Navbar() {
         };
     }, []);
 
+    const currentUser = {
+        id: 1,
+        username: "John",
+        isSeller: true
+    }
+
     return (
-        <div className={active ? 'fiverr-navbar active' : 'fiverr-navbar'}>
+        <div className={active || pathname !== '/' ? 'fiverr-navbar active' : 'fiverr-navbar'}>
             <div className='fiverr-container'>
                 <div className='fiverr-navbar-logo'>
-                    {/* <Link to="/"> */}
-                    <span className='fiverr-logo'>
-                        fiverr
-                    </span>
-                    <span className='fiverr-dot'>
-                        .
-                    </span>
-                    {/* </Link> */}
+                    <Link to="/" className='link'>
+                        <span className='fiverr-logo'>
+                            fiverr
+                        </span>
+                        <span className='fiverr-dot'>
+                            .
+                        </span>
+                    </Link>
                 </div>
                 <div className='fiverr-navbar-links'>
                     <span>Fiverr Business</span>
                     <span>Explore</span>
                     <span>⨁ English</span>
                     <span>PKR</span>
-                    <span>Become a seller</span>
+                    {!currentUser.isSeller && <span>Become a seller</span>}
                     <span>Sign In</span>
-                    <button className={activeBtn ? 'active' : ''}>Join</button>
-
+                    {!currentUser && <button>Join</button>}
+                    {
+                        currentUser && (
+                            <div className='user' onClick={() => setOpen(!open)}>
+                                <img src="https://yt3.ggpht.com/2s_1CeNUitllfxhKzP_o4adud90fvR5tvoLRTqdLL5GFrSZhsFJWCSxC4CqySMMK85Jhv1Re=s88-c-k-c0x00ffffff-no-rj-mo" alt='profile' />
+                                <span>{currentUser.username}</span>
+                                {open && <div className='options'>
+                                    {
+                                        currentUser.isSeller && (
+                                            <>
+                                                <Link className='link' to="/mygigs">Gigs</Link>
+                                                <Link className='link' to="/addnewgig">Add new Gig</Link>
+                                            </>
+                                        )
+                                    }
+                                    <Link className='link' to="/orders">Orders</Link>
+                                    <Link className='link' to="/messages">Messages</Link>
+                                    <Link className='link'>Logout</Link>
+                                </div>}
+                            </div>
+                        )
+                    }
                 </div>
             </div>
 
             {
-                active &&
-                <>
-                    <div className='fiverr-navbar-hr'></div>
-                    <div className='fiverr-menu'>
-                        <div>Test</div>
-                        <div>Test</div>
-                    </div>
-                </>
+                (active || pathname !== '/') && (
+                    <>
+                        <div className='fiverr-navbar-hr'></div>
+                        <div className='fiverr-menu'>
+                            <Link to='/' className='link'>Graphics & Design</Link>
+                            <Link to='/' className='link'>Video & Animation</Link>
+                            <Link to='/' className='link'>Writing & Translation</Link>
+                            <Link to='/' className='link'>AI Services</Link>
+                            <Link to='/' className='link'>Digital Marketing</Link>
+                            <Link to='/' className='link'>Music & Audio</Link>
+                            <Link to='/' className='link'>Programming & Tech</Link>
+                            <Link to='/' className='link'>Business</Link>
+                            <Link to='/' className='link'>Lifestyle</Link>
+                        </div>
+                    </>
+                )
             }
         </div>
     );
