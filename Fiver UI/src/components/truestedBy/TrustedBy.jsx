861b1012e0ab096d92f3319ab862d387b50d0c69
Arham Javed <81445694+arhamjaved46@users.noreply.github.com>
2023-02-27 04:57:43 +0500

    Section completed trusted by companies

---
+++ Fiver UI/src/components/truestedBy/TrustedBy.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './TrustedBy.scss';
+
+function TrustedBy() {
+    return (
+        <div className='fiverr-trustedBy'>
+            <div className='fiverr-trustedBy-container'>
+                <span>Trusted By:</span>
+                <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/facebook2x.188a797.png" alt="Facebook" />
+                <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/google2x.06d74c8.png" alt="Google" />
+                <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/netflix2x.887e47e.png" alt="Netflix" />
+                <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/pandg2x.6dc32e4.png" alt="P&G" />
+                <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/paypal2x.22728be.png" alt="paypal" />
+            </div>
+        </div>
+    )
+}
+
+export default TrustedBy;
--- Fiver UI/src/pages/homepage/homepage.jsx
+++ Fiver UI/src/pages/homepage/homepage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Featured from '../../components/featured/Featured';
+import TrustedBy from '../../components/truestedBy/TrustedBy';
 import './homepage.scss';
 
 function homepage() {
   return (
     <div className='homepage'>
       <Featured />
+      <TrustedBy />
     </div>
   )
 }
