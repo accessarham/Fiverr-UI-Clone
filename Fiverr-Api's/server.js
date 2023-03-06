49d122c19ad59416f5dfa87dc64638595b389c3c
Arham Javed <81445694+arhamjaved46@users.noreply.github.com>
2023-03-06 20:42:20 +0500

    Create Express Api Structure

--- Fiverr-Api's/server.js
+++ Fiverr-Api's/server.js
@@ -1,6 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import conversationRoute from "./routes/conversation.route.js";
+import gigRoute from "./routes/gig.route.js";
+import messageRoute from "./routes/message.route.js";
+import orderRoute from "./routes/order.route.js";
+import reviewRoute from "./routes/review.route.js";
+import userRoute from "./routes/user.route.js";
 
 const app = express();
 
@@ -14,6 +20,13 @@ const connect = async () => {
         console.log(error);
     }
 }
+
+app.use('/api/conversations', conversationRoute);
+app.use('/api/gigs', gigRoute);
+app.use('/api/messages', messageRoute);
+app.use('/api/orders', orderRoute);
+app.use('/api/reviews', reviewRoute);
+app.use('/api/users', userRoute);
 
 app.listen(8800, () => {
     connect();
